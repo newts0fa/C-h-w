525e9dad5af29ec7ba78c7b75d6fc9a7ece86580
nbtvo <nbtvo@DESKTOP-II66ME9>
2024-11-03 23:42:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <limits>
+using namespace std;
+int main()
+{
+	int s;
+	int x;
+	int y;
+	cin >> x;
+	cin >> y;
+	double result;
+
+	result = 0.125 * abs((sin(x)/y));
+	cout  << result << endl;
+
+	float i, j, res;
+	cin >> i;
+	cin >> j;
+	res = pow(2, j);
+	double lg = log10(res);
+	cout << lg << endl;
+
+
+	cout << fmax(result,lg) << endl;
+
+
+}
